b15850717225be7a51f7d7b71c3bf79a1a8e7a21
Tamim <jobaertamim7@gmail.com>
2025-04-16 22:13:31 +0600

    NZR-I decoding done

--- nzr-I.py
+++ nzr-I.py
@@ -6,11 +6,20 @@ String : TypeAlias = str
 
 class NRZ_I:
 
-    def __init__(self, message : String):
+    def __init__(self, message : String = '', signal : String = '', time_period : Integer = 1):
         self.message : String = message
+        self.signal : List[Integer] = list(int(bit) for bit in signal)   
+        self.time_period = time_period
+
+
+    def setMessage(self, msg: String):
+        self.message = msg
+
+    def setSignal(self, signal : String):
+        self.signal = list(int(bit) for bit in signal)
+
 
     def encode(self) -> List[Integer]:
-        
         encoded_signal : List[String] = []
         signal_level = 1
 
@@ -21,10 +30,24 @@ class NRZ_I:
 
         return encoded_signal 
     
+    
     def decode(self) -> String:
-         pass
+        decoded_msg : String = ""
+        if self.signal == 0:
+           decoded_msg = decoded_msg + "0"
+        else:
+           decoded_msg = decoded_msg + "1"
+           
+        for i in range(0, len(self.signal)-1):
+            # print(f'iteration no {i} : {self.signal[i]} {self.signal[i+1]}')
 
-
+            if  self.signal[i] == self.signal[i+1]:
+               decoded_msg = decoded_msg + "0"
+            else:
+               decoded_msg = decoded_msg + "1"
+        return decoded_msg
+    
+    
     def plot(self):
             signal = self.encode()
             time : List[Integer] = []
@@ -32,24 +55,30 @@ class NRZ_I:
             i = 0 
 
             for level in signal:
-                time.extend([i for i in range(i,i+3)])
-                levels.extend([level]*3)
+                time.extend([i for i in range(i,i+self.time_period+1)])
+                levels.extend([level]*(self.time_period+1))
 
-                i = i+2
+                i = i+self.time_period
 
-            print(time)
-            print(levels)
+
             plt.figure(figsize=(15,7))
             plt.ylabel("Signal")
+            plt.yticks([0,1])
             plt.xlabel("Time")
+            plt.xticks([i for i in range(0, len(self.message)*self.time_period+1)])
             plt.plot(time,levels)
+            plt.grid(True)
             plt.show()
 
 if __name__ == '__main__':
     input_bits = '101101010'
-    nrzi = NRZ_I(input_bits)
-    print("Encoded Signal:", nrzi.encode())
+    nrzi = NRZ_I(message=input_bits,time_period=3)
+    nrzi.setSignal(nrzi.encode())
+    print(nrzi.encode())
+    print(nrzi.decode())
     nrzi.plot()
+    
+
 
 
 
